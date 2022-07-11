acdfcb5454c51026d94804c2660aa44c97937336
Ozum Safaoglu <ozum.safaoglu@truelayer.com>
2022-07-11 23:26:08 +0100

    Argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 mod client;
 pub mod commands;
 
-fn main() {
+use std::fs;
+use clap::{arg, Parser};
 
+#[derive(Parser, Debug)]
+struct Args {
+    #[clap(short, long, value_parser)]
+    private_key: String,
+    #[clap(short, long, value_parser)]
+    mode: String,
+}
+
+fn main() {
+    let args = Args::parse();
+    let contents = fs::read_to_string(&args.private_key)
+        .expect("Could not read the private key file");
+    println!("{}", args.private_key);
+    println!("{}", contents);
 }
